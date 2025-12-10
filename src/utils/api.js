cbf9ad05fcedb17401f4c28dbd0ba25308790a9d
Muqadas-javed <muqadasjaved1197@gmail.com>
2025-12-10 17:40:21 +0500

    Added global padding/margin changes to website

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://eventra-backend-vzlg.onrender.com';
+
 
 // Create axios instance
 const api = axios.create({
