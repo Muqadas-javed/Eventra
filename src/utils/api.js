38167af7f0f6b7600ca10793283cb17f746100c9
Muqadas-javed <muqadasjaved1197@gmail.com>
2025-12-12 11:16:52 +0500

    Update API URL for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://eventra-backend-vzlg.onrender.com';
+// const API_BASE_URL = 'http://localhost:5000/api';
+
+const API_BASE_URL = 'https://eventra-backend-vzlg.onrender.com';
 
 
 // Create axios instance
