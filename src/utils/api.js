739e98bbe8810a22f0dc444d14fd010c833a4d68
Muqadas-javed <muqadasjaved1197@gmail.com>
2025-12-12 11:26:55 +0500

    Fix API base URL - add /api path

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
 
-const API_BASE_URL = 'https://eventra-backend-vzlg.onrender.com';
+const API_BASE_URL = 'https://eventra-backend-vzlg.onrender.com/api';
 
 
 // Create axios instance
